10b6c243f5c0e34ee8636752e12258c5e2073ec1
ombasa <ombasaherman@gmail.com>
2022-04-07 23:23:29 +0300

    Note page

---
+++ src/pages/NotePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import notes from '../assets/data';
+
+
+const NotePage = ({match}) => {
+  let noteId = match.params.id;
+  
+  let note = notes.find(note => note.id == noteId)
+  return (
+    <div>
+        <p>{note?.body}</p>
+    </div>
+  )
+}
+
+export default NotePage;
